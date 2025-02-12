8eae7d9a27f6b82f8310f8c72be28582bab4b776
Winexpert <winexpert2021@gmail.com>
2025-02-12 17:43:33 +0530

    Create Androidmanifest.java

---
+++ Androidmanifest.java
@@ -0,0 +1,6 @@
+  <activity
+            android:name=".cast.ExpandedControlsActivity"
+            android:exported="false" />
+  <meta-data
+            android:name="com.google.android.gms.cast.framework.OPTIONS_PROVIDER_CLASS_NAME"
+            android:value="com.primemedia.tutorial.cast.CastOptionsProvider" />   -> dont forget to change the package name here
